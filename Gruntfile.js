68ee94b9c31a912d38648a1b35eeac98c4fdbb42
yuzuemon <killerqueen64@gmail.com>
2014-07-28 12:17:16 +0900

    1章終わりまで

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+// 'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+
+    connect: {
+      server: { //ぶっちゃけここの名前何でもいい.
+        options: {
+          hostname: 'localhost',
+          port: 9001 // default 35729
+        }
+      }
+    },
+
+    watch: {
+      livereload: {
+        files: ['**/*.html', '**/*.js', '**/*.css'],
+        options: {
+          livereload: true,
+          spawn: false
+        }
+      }
+    }
+
+  });
+
+  // load task
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task
+  grunt.registerTask('default', ['connect', 'watch']);
+};
---
+++ controllers.js
@@ -0,0 +1,3 @@
+function HelloController($scope, $location) {
+  $scope.greeting = { text: 'Hello' };
+}
